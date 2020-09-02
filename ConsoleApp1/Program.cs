165165a77cde4f7e8bfbcdc288c1ab3bd4f33fa0
Otabek Qayumov <tm_gambit@live.ru>
2020-09-02 19:39:13 +0500

    translit va ovoz bilan ishlash kiritildi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -31,9 +33,37 @@ namespace ConsoleApp1
 				result.Add(Num2Text(numbers[i], j));
 			}
 			Console.WriteLine(string.Join(" ", result.ToArray()));*/
+			string str = "ён";
+			string[] lat_low = { "a", "b", "v", "g", "d", "e", "yo", "j", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "x", "ts", "ch", "sh", "\'", "e", "yu", "ya" };
+			string[] rus_low = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "ъ", "э", "ю", "я" };
+			for (int i = 0; i < 30; i++)
+			{
+				str = str.Replace(rus_low[i], lat_low[i]);
+			}
+			Console.WriteLine(str);
 
-			Dictionary<string, string[]> dict = new Dictionary<string, string[]>();
-			Console.WriteLine(JsonConvert.SerializeObject(dict));
+			/* ovoz bilan ishlash
+			SoundPlayer player = new SoundPlayer();
+			Dictionary<string, List<string>> audioPaths = null;
+			using (var sr = File.OpenText("AudioPath.txt"))
+			{
+				string paths = sr.ReadToEnd();
+				audioPaths = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(paths);
+				for (int i = 0; i < audioPaths.Count; i++)
+				{
+					foreach (List<string> item in audioPaths.Values)
+					{
+						player.SoundLocation = item[0];
+						player.Play();
+						Thread.Sleep(100);
+					}
+				}
+			}
+			player.Dispose();*/
+
+			/* // audio bazani shakllantirish
+			Dictionary<string, List<string>> templist = new Dictionary<string, List<string>>();
+			//Console.WriteLine(JsonConvert.SerializeObject(dict));
 			List<string> list = new List<string>();
 			using (var sr = File.OpenText("paths.txt"))
 			{
@@ -41,19 +71,26 @@ namespace ConsoleApp1
 				for (int i = 0; i < paths.Length; i++)
 				{
 					var s = paths[i].Split('_');
-					var templist = new List<string>();
-					var lastslog = s[0];
-					if (i == 0)
+					//var templist = new Dictionary<string, List<string>>();
+					string name = s[0], path = s[1];
+					if (i == 0 || !templist.ContainsKey(s[0]))
 					{
-						dict.Add(s[0],)
+						templist.Add(s[0], new List<string>() { @"Data\" + paths[i] });
+						continue;
+					}
+					if (templist.ContainsKey(s[0]))
+					{
+						templist[s[0]].Add(@"Data\" + paths[i]);
 					}
 				}
 			}
+			var result = JsonConvert.SerializeObject(templist);
+			Console.WriteLine(result);
 			using (var sw = File.CreateText("AudioPath.txt"))
 			{
-				
+				sw.Write(result);
 			}
-
+			*/
 			Console.ReadKey();
 		}
 
